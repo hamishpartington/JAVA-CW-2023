ac644e38fb895e754c02b15235d407baf6ad7f92
hamishpartington <hamishpartington25@gmail.com>
2024-02-09 11:27:33 +0000

    Finished workbook 3

--- cw-shapes/src/main/java/edu/uob/Shapes.java
+++ cw-shapes/src/main/java/edu/uob/Shapes.java
@@ -2,52 +2,64 @@ package edu.uob;
 
 public class Shapes {
 
-   public static void main(String[] args) {
-    TwoDimensionalShape myTriangle = new Triangle(40, 40, 40);
-    myTriangle.setShapeColour(Colour.BLUE);
-    System.out.println(myTriangle);
-    TwoDimensionalShape myRectangle = new Rectangle(20, 40);
-    myRectangle.setShapeColour(Colour.RED);
-    System.out.println(myRectangle);
-    TwoDimensionalShape myCircle = new Circle(10);
-    myCircle.setShapeColour(Colour.GREEN);
-    System.out.println(myCircle);
-    Triangle myTriangle2 = new Triangle(4, 4, 4);
-    System.out.println(myTriangle2.getVariant());
-    System.out.println(myTriangle2.calculateArea());
-    System.out.println(myTriangle2.calculatePerimeterLength());
-    Triangle myTriangle3 = new Triangle(4, 5, 4);
-    System.out.println(myTriangle3.getVariant());
-    System.out.println(myTriangle3.calculateArea());
-    System.out.println(myTriangle3.calculatePerimeterLength());
-    Triangle myTriangle4 = new Triangle(33, 33, 33);
-    System.out.println(myTriangle4.getVariant());
-    System.out.println(myTriangle4.calculateArea());
-    System.out.println(myTriangle4.calculatePerimeterLength());
-    Triangle myTriangle5 = new Triangle(6, 4, 6);
-    System.out.println(myTriangle5.getVariant());
-    System.out.println(myTriangle5.calculateArea());
-    System.out.println(myTriangle5.calculatePerimeterLength());
+ public static void main(String[] args) {
+  TwoDimensionalShape myTriangle = new Triangle(40, 40, 40);
+  myTriangle.setShapeColour(Colour.BLUE);
+  System.out.println(myTriangle);
+  TwoDimensionalShape myRectangle = new Rectangle(20, 40);
+  myRectangle.setShapeColour(Colour.RED);
+  System.out.println(myRectangle);
+  TwoDimensionalShape myCircle = new Circle(10);
+  myCircle.setShapeColour(Colour.GREEN);
+  System.out.println(myCircle);
+  Triangle myTriangle2 = new Triangle(4, 4, 4);
+  System.out.println(myTriangle2.getVariant());
+  System.out.println(myTriangle2.calculateArea());
+  System.out.println(myTriangle2.calculatePerimeterLength());
+  Triangle myTriangle3 = new Triangle(4, 5, 4);
+  System.out.println(myTriangle3.getVariant());
+  System.out.println(myTriangle3.calculateArea());
+  System.out.println(myTriangle3.calculatePerimeterLength());
+  Triangle myTriangle4 = new Triangle(33, 33, 33);
+  System.out.println(myTriangle4.getVariant());
+  System.out.println(myTriangle4.calculateArea());
+  System.out.println(myTriangle4.calculatePerimeterLength());
+  Triangle myTriangle5 = new Triangle(6, 4, 6);
+  System.out.println(myTriangle5.getVariant());
+  System.out.println(myTriangle5.calculateArea());
+  System.out.println(myTriangle5.calculatePerimeterLength());
 
-    // array of shapes
-    TwoDimensionalShape[] TwoDShapesA = new TwoDimensionalShape[100];
-    int nTriangles = 0, nRectangles = 0, nCircles = 0;
-    for(int i = 0; i < 100; i++) {
-     double r = Math.random();
-     if(r < 0.333){
-      TwoDShapesA[i] = new Triangle(4, 4, 4);
-      nTriangles++;
-     } else if (r < 0.666){
-      TwoDShapesA[i] = new Circle(4);
-      nCircles++;
-     } else {
-      TwoDShapesA[i] = new Rectangle(4, 4);
-      nRectangles++;
-     }
-     TwoDShapesA[i].setShapeColour(Colour.BLUE);
-     System.out.println(TwoDShapesA[i].toString());
-    }
-    System.out.println("No. Triangles = " + nTriangles + "\nNo. Circles = " + nCircles + "\nNo. Rectangles = " +nRectangles);
+  // array of shapes
+  TwoDimensionalShape[] TwoDShapesA = new TwoDimensionalShape[100];
+  int nTriangles = 0, nRectangles = 0, nCircles = 0;
+  for(int i = 0; i < 100; i++) {
+   double r = Math.random();
+   if(r < 0.333){
+    TwoDShapesA[i] = new Triangle(4, 4, 4);
+    nTriangles++;
+   } else if (r < 0.666){
+    TwoDShapesA[i] = new Circle(4);
+    nCircles++;
+   } else {
+    TwoDShapesA[i] = new Rectangle(4, 4);
+    nRectangles++;
+   }
+   TwoDShapesA[i].setShapeColour(Colour.BLUE);
+   System.out.println(TwoDShapesA[i].toString());
   }
+  System.out.println("No. Triangles = " + nTriangles + "\nNo. Circles = " + nCircles + "\nNo. Rectangles = " +nRectangles);
+
+  for(int i = 0; i < 100; i++) {
+   if(TwoDShapesA[i] instanceof Triangle) {
+    System.out.println("No. of Triangles = " + Triangle.getNTriangles());
+    break;
+   }
+  }
+  for(int i = 0; i < 100; i++){
+   if(TwoDShapesA[i] instanceof Triangle currShape){
+    System.out.println(currShape.getVariant());
+   }
+  }
+ }
 }
 
--- cw-shapes/src/main/java/edu/uob/Triangle.java
+++ cw-shapes/src/main/java/edu/uob/Triangle.java
@@ -3,8 +3,10 @@ package edu.uob;
 public class Triangle extends TwoDimensionalShape implements MultiVariantShape{
 
   long side1, side2, side3;
+  static int nTriangles = 0;
   TriangleVariant tV;
   public Triangle(long side1, long side2, long side3) {
+    Triangle.nTriangles++;
     this.side1 = side1;
     this.side2 = side2;
     this.side3 = side3;
@@ -31,6 +33,9 @@ public class Triangle extends TwoDimensionalShape implements MultiVariantShape{
     return Math.max(side1, Math.max(side2, side3));
   }
 
+  public static int getNTriangles(){
+    return Triangle.nTriangles;
+  }
   public String toString(){
     return super.toString() + " " + tV +" triangle with sides length " + side1 + ", " + side2 + ", " + side3;
   }
