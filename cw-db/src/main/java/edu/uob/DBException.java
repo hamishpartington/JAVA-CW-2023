b0dfeb482f820cd7e78750f84e409e8e45f53d87
Hamish Partington <hamishpartington25@gmail.com>
2024-03-15 18:46:39 +0000

    Add serials to exceptions to allow commandline compilation

--- cw-db/src/main/java/edu/uob/DBException.java
+++ cw-db/src/main/java/edu/uob/DBException.java
@@ -1,52 +1,62 @@
 package edu.uob;
 
+import java.io.Serial;
+
 public class DBException extends Exception {
     public DBException(String message) {
         super(message);
     }
-
+    @Serial private static final long serialVersionUID = 1;
     public static class TableAlreadyExists extends DBException {
+        @Serial private static final long serialVersionUID = 1;
         public TableAlreadyExists(String tableName, String databaseName) {
             super(tableName + " already exists in the " + databaseName + " database");
         }
     }
     public static class TableDoesNotExist extends DBException {
+        @Serial private static final long serialVersionUID = 1;
         public TableDoesNotExist (String tableName, String databaseName) {
             super("The table: " + tableName + " does not exist in the " + databaseName + " database");
         }
     }
 
     public static class DBAlreadyExists extends DBException {
+        @Serial private static final long serialVersionUID = 1;
         public DBAlreadyExists(String DBName) {
             super("The database: " + DBName + " already exists");
         }
     }
 
     public static class DBDoesNotExist extends DBException {
+        @Serial private static final long serialVersionUID = 1;
         public DBDoesNotExist(String DBName) {
             super("The database: " + DBName + " does not exist");
         }
     }
 
     public static class incorrectNumberOfValues extends DBException {
+        @Serial private static final long serialVersionUID = 1;
         public incorrectNumberOfValues(String tableName, int size) {
             super("Incorrect number of values! The table: " + tableName + " has " + size + " fields");
         }
     }
 
     public static class duplicateFields extends DBException {
+        @Serial private static final long serialVersionUID = 1;
         public duplicateFields() {
             super("All fields must be unique");
         }
     }
 
     public static class fieldDoesNotExist extends DBException {
+        @Serial private static final long serialVersionUID = 1;
         public fieldDoesNotExist(String field) {
             super("Cannot SELECT " + field + " as it does not exist");
         }
     }
 
     public static class cannotRemoveID extends DBException {
+        @Serial private static final long serialVersionUID = 1;
         public cannotRemoveID() {
             super("The id field cannot be removed");
         }
--- cw-db/src/main/java/edu/uob/ParserException.java
+++ cw-db/src/main/java/edu/uob/ParserException.java
@@ -1,146 +1,174 @@
 package edu.uob;
 
+import java.io.Serial;
+
 public class ParserException extends Exception {
+    @Serial
+    private static final long serialVersionUID = 1;
     public ParserException(String message) {
         super(message);
     }
 
     public static class QueryNotTerminated extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public QueryNotTerminated() {
             super("Query must be terminated with semicolon");
         }
     }
 
     public static class NotACommandType extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public NotACommandType(String token) {
             super(token + " is not a valid command type");
         }
     }
 
     public static class InvalidDatabaseName extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public InvalidDatabaseName(String token) {
             super(token + " is not a valid database name. Must use alphanumeric characters with no spaces.");
         }
     }
     public static class InvalidTableName extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public InvalidTableName (String token) {
             super(token + " is not a valid table name. Must use alphanumeric characters with no spaces.");
         }
     }
 
     public static class InvalidAttributeName extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public InvalidAttributeName  (String token) {
             super(token + " is not a valid attribute name. Must use alphanumeric characters with no spaces.");
         }
     }
 
     public static class InvalidCreate extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public InvalidCreate (String token) {
             super("Can only create a table or database, not " + token);
         }
     }
 
     public static class InvalidDrop extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public InvalidDrop (String token) {
             super("Can only drop a table or database, not " + token);
         }
     }
 
     public static class InvalidAlter extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public InvalidAlter () {
             super("Can only alter a table");
         }
     }
 
     public static class NoInto extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public NoInto () {
             super("Need INTO after INSERT");
         }
     }
 
     public static class NoValues extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public NoValues () {
             super("Need VALUES after table name in INSERT statement");
         }
     }
 
     public static class NoValueList extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public NoValueList () {
             super("Need list of values in parentheses after VALUES in INSERT statement");
         }
     }
     public static class InvalidAlterationType extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public InvalidAlterationType (String token) {
             super("Can only ADD or DROP in ALTER command. Not:" + token);
         }
     }
     public static class ListNotTerminated extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public ListNotTerminated(String terminator, String type) {
             super(type + " list must be terminated with " + terminator);
         }
     }
 
     public static class InvalidValue extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public InvalidValue(String token) {
             super(token + " is not a valid value.");
         }
     }
 
     public static class InvalidSelect extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public InvalidSelect(String token) {
             super("Invalid SELECT statement! Next word in SELECT statement should be ; or WHERE not " + token);
         }
     }
 
     public static class UnmatchedParentheses extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public UnmatchedParentheses() {
             super("WHERE clause contains conditions with unmatched parentheses");
         }
     }
 
     public static class InvalidComparator extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public InvalidComparator(String token) {
             super(token + " is not a valid comparator");
         }
     }
 
     public static class InvalidStatementSyntax extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public InvalidStatementSyntax(String token, String statement) {
             super(token + " is not a valid syntax for " + statement + " statement");
         }
     }
 
     public static class NoSetInUpdate extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public NoSetInUpdate(String token) {
             super("Expected SET not " + token);
         }
     }
 
     public static class InvalidNameValuePair extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public InvalidNameValuePair() {
             super("Name value pair should be in Attribute = Value format");
         }
     }
 
     public static class NoFromDelete extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public NoFromDelete(String token) {
             super("Expected DELETE FROM not DELETE " + token);
         }
     }
 
     public static class NoWhere extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public NoWhere(String token) {
             super("Expected WHERE not " + token);
         }
     }
 
     public static class InvalidJoin extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public InvalidJoin(String token, String expected) {
             super("Invalid JOIN! Expected " + expected + " not " + token);
         }
     }
 
     public static class NoDatabaseInUse extends ParserException {
+        @Serial private static final long serialVersionUID = 1;
         public NoDatabaseInUse(String statementType) {
             super("Database must be in use to " + statementType + " tables");
         }
